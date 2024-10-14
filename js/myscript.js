615feb4caf53747b17d6e6c7c8e2b70ba4bb350c
robchiksg06 <120631254+robchiksg06@users.noreply.github.com>
2024-10-14 20:34:52 +0000

    darkmodes

--- js/myscript.js
+++ js/myscript.js
@@ -1,8 +1,23 @@
-// Get the checkbox element
-const toggleSwitch = document.getElementById('darkModeToggle');
+const toggleSwitch = document.getElementById('slider-input');
+const aside = document.getElementById('aside')
 
-// Add an event listener to handle the change event
+if(localStorage.getItem('theme') === 'dark'){
+  document.body.classList.add('dark-mode');
+  aside.classList.add('aside-dark-mode')
+  toggleSwitch.checked = true
+}
+
+
 toggleSwitch.addEventListener('change', () => {
-    // Toggle the dark-mode class on the body
-    document.body.classList.toggle('dark-mode');
-});
+  if(toggleSwitch.checked){
+    document.body.classList.add('dark-mode')
+    localStorage.setItem('theme', 'dark')
+    aside.classList.add('aside-dark-mode')
+
+
+  }else{
+    document.body.classList.remove('dark-mode')
+    localStorage.setItem('theme', 'light')
+    aside.classList.remove('aside-dark-mode')
+  }
+})
